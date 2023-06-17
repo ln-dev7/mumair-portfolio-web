7b63b34f1fa7dfee33e675370401a75fb624ded1
LN <leonelngoya@gmail.com>
2023-06-17 21:14:29 +0100

    header responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+"use client"
+
+import { useState } from "react";
 import "./page.scss";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className="main">
       <header className="header">
@@ -10,7 +14,7 @@ export default function Home() {
               <img src="/logo.svg" alt="logo" />
             </a>
           </div>
-          <div className="header-right">
+          <div className={`${isOpen ? "header-right active" : "header-right"}`}>
             <ul>
               <li>
                 <a href="#">Home</a>
@@ -31,7 +35,27 @@ export default function Home() {
                 <a href="#">Contact</a>
               </li>
             </ul>
-            <a className="cv" href="#">Download CV</a>
+            <a className="cv" href="#">
+              Download CV
+            </a>
+          </div>
+          <div className="header-burger"
+          onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
           </div>
         </div>
       </header>
